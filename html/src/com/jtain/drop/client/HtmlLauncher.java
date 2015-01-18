8b6d170c086d3c1f36104a967b360a53f9473084
Jim Tain <jtain@lmp-jtain01.yutain>
2015-01-18 16:50:20 -0600

    Initial Commit of Drop test game

---
+++ html/src/com/jtain/drop/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jtain.drop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jtain.drop.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 480);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Drop();
+        }
+}
